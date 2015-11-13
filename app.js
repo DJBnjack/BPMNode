55afea07614b8afee88c471fd7f8cb2a19fcbfe8
Johannes Bertens <johannes.bertens@live.nl>
2015-11-13 11:27:50 +0100

    Disabled cache for testing

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', handlebars({defaultLayout:'main'}));
 app.set('view engine', 'handlebars');
 
+// Disable cache
+app.disable('etag');
+
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
